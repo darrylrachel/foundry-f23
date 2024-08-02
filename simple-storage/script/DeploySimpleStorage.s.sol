f94696b994caa173bfa67409118cc8109867eedf
Darryl Rachel <racheldarryl@icloud.com>
2024-08-02 13:09:58 -0700

    Encrypted p.key

--- simple-storage/script/DeploySimpleStorage.s.sol
+++ simple-storage/script/DeploySimpleStorage.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeploySimpleStorage is Script {
+contract DeploySimpleStorage is Script{
 
   function run() external returns (SimpleStorage) {
     vm.startBroadcast();
